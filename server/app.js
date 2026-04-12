cff016d4ef9b2b341f780907d1d1a5aea4969fc9
yosef ohayon <yyo6420@gmail.com>
2026-04-12 16:08:09 +0300

    mongo connection

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import experss from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { makemongoConnection } from "./src/mongodb/mongodb.js";
+
+const app = experss();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("tiny"));
+
+app.use(experss.json());
+
+app.use(experss.urlencoded({ extended: true }));
+
+app.use(cors());
+
+await makemongoConnection();
+
+app.get("/", async (request, response) => {
+    response.json({
+        message: "Welcome to Flighter Aircraft Management API",
+        version: "1.0.0",
+    });
+});
+
+app.listen(PORT, async () => {
+    console.log(`listening on port ${PORT}...`);
+});
---
+++ server/src/mongodb/mongodb.js
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+
+let mongoconnection;
+
+export const makemongoConnection = async () => {
+    if (mongoconnection) return mongoconnection;
+    try {
+        if (!process.env.MONGO_URI) {
+            throw new Error("MONGO_URI is not exist in .env file");
+        };
+        mongoconnection = new MongoClient(process.env.MONGO_URI);
+        await mongoconnection.connect();
+        console.log("Connection is established successfully ;)");
+        return mongoconnection;
+    } catch (error) {
+        mongoconnection = null;
+        console.error(error.message);
+        throw error;
+    };
+};
+
+export const getdb = async () => {
+    if (!mongoconnection) await makemongoConnection();
+    if (!mongoconnection) throw new Error("Database connection is not established. Check your MongoDB URI.")
+    return mongoconnection?.db(process.env.DB_NAME);
+};
+
+export const closeConnection = async () => {
+    if (!mongoconnection) return;
+    await mongoconnection.close();
+    mongoconnection = null;
+    console.log("MongoDB connection closed safely, bye bye ;)")
+};
+
+export const db = await getdb(process.env.DB_NAME);
